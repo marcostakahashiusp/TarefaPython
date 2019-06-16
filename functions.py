0dec66ebc1d688cfa4b7aed38d1c641fae7c8467
marcostakahashiusp <marquinhotakahashi@usp.br>
2019-06-16 08:12:59 -0300

    Funcao adicionar numeros a lista implementada

--- functions.py
+++ functions.py
@@ -10,3 +10,21 @@ def search_value(key, list):
 def show(list):
     for i in list:
         print(i)
+    return None
+
+def insert_value(list):
+    while True:
+        value = input("Digite um valor a ser adicionado ou end para sair: ")
+        if value == "end":
+            break
+        else:
+            try:
+                newValue = float(value)
+            except:
+                print("O comando digitado não é um número")
+            if search_value(newValue, list):
+                print("Esse valor já está na lista")
+            else:
+                list.append(newValue)
+                list.sort()
+    return None
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from functions import search_value, show
+from functions import search_value, show, insert_value
 list = [1, 4, 3, 2, 7, 1.5]
 list.sort()
+show(list)
+insert_value(list)
 show(list)
